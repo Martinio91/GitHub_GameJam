ae8eab20cd24eed51f8a28673d7608c974e916b6
MartinioS <164780280+Martinio91@users.noreply.github.com>
2025-02-22 15:54:15 +0000

    Added new scene
    This scene will be in more detail and will contain a larger level

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -135,7 +135,7 @@ public class PlayerController : MonoBehaviour
 
         numberJumpSinceGrounded++;
 
-        if (onWall)
+        if (onWall && !onGround)
         {
             dirToWall = wallCheck.GetDirectionToColliders(tr);
             velocity = rb.velocity;
